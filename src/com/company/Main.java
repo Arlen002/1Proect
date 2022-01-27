53e072d561d5872c9615eee4daceda962a6ce541
Arlen002 <kurbanbaevarlen001@gmail.com>
2022-01-27 14:15:17 +0300

    1 Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+  public class Main {
+      public static void main(String[] args) {
+
+
+
+
+
+
+      }
+
+          
+  }
+
+//      Scanner scanner = new Scanner(System.in);                                                                                                  System.out.println("Консольго эки сан жазыныз: ");
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        if(a % b==0){
+//        System.out.println(a+" "+b+"ге калдыксыз болунот");                                                                                                                            } else {
+//        System.out.println(a+" "+b+"ге болунбойт");
+//        }
+//        }
+           /*  int shet = 0;
+                 for (int i = 1; i <= 300 ; i++) {
+                  if (i % 3 == 0 || i % 4 == 0 || i % 5 == 0){
+                  shet = shet + 1;
+                  }
+                  }
+                  System.out.println(shet);
+*/
